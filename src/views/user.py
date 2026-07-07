d6693e6d7af5e6ac9268cc6673eda3ab7b088bb3
AlienneSilva <88863077+AlienneSilva@users.noreply.github.com>
2026-07-07 09:21:50 -0300

    Configura o Marshmallow

--- src/views/user.py
+++ src/views/user.py
@@ -1,20 +1,16 @@
 from src.app import ma
 from src.views.role import RoleSchema
+from src.models.user import Usuario
 from marshmallow import fields
 
-class UsuarioSchema(ma.Schema):
-    id = fields.Int(dump_only=True)
-    username = fields.Str(required=True)
-    role_id = fields.Int()
-    # Declarado aqui em cima para o Meta conseguir enxergar!
-    role = fields.Nested(RoleSchema) 
-
+class UsuarioSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ("id", "username", "role")
+        model = Usuario
+        include_fk = True
 
 
 class CreateUserSchema(ma.Schema):
     username = fields.Str(required=True)
     password = fields.Str(required=True)
-    role_id = fields.Int(required=True)
+    role_id = fields.Int(required=True, strict=True)
 
